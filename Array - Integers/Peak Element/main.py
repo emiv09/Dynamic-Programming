0703d6109a5b7c1a102fb6606866e494676d661a
Emilian <emilianv09@gmail.com>
2025-03-07 16:35:11 +0200

    Find Peak Element

---
+++ Array - Integers/Peak Element/main.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        left = 0 
+        right = len(nums)
+
+        while left < right:
+            mid = (right-left) // 2
+
+            if nums[mid] > nums[mid+1]:
+                right = mid
+
+            else: 
+                left = mid+1
+        
+        return left
+    
+solution = Solution()
+print(solution.findPeakElement([1,2,3,1]))
