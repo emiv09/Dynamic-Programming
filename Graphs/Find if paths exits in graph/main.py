d9e4be3af7527dbbb6d4b2a6f81b10cd6d38d596
Emilian <emilianv09@gmail.com>
2025-03-17 22:35:48 +0200

    Valid Path in Graph

--- Graphs/Find if paths exits in graph/main.py
+++ Graphs/Find if paths exits in graph/main.py
@@ -1,5 +1,7 @@
+from collections import defaultdict
+
 class Solution:
-    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+    def validPath(self, n: int, edges: list[list[int]], source: int, destination: int) -> bool:
         graph = defaultdict(list)
 
         for u,v in edges:
